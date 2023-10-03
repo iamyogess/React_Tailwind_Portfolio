b64395b1380c0c2f01dffb6f1693cedb041d076b
Yogesh <iamyogess@gmail.com>
2023-10-03 17:49:16 +0545

    added progress bar

--- frontend/src/components/ProgressBar.jsx
+++ frontend/src/components/ProgressBar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const ProgressBar = () => {
-  return (
-    <div>ProgressBar</div>
-  )
-}
+  const [scollTop, setScrollTop] = useState();
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
-export default ProgressBar
+  const onScroll = () => {
+    const winScroll = document.documentElement.scrollTop; //scrolled height
+    const height = document.documentElement.clientHeight; //total height
+    const scrolled = (winScroll / height) * 100;
+    setScrollTop(scrolled);
+  };
+
+  return (
+    <div className="w-1.5 bg-gray-200 fixed ml-[-5px] mt-[90px] h-full mb-4">
+      <div className="bg-green-600 transition-all ease-out duration-1000" style={{height:`${scollTop}%`}}></div>
+    </div>
+  );
+};
+
+export default ProgressBar;
